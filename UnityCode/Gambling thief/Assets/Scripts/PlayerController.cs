968ecfc2e953de6b12510823630b4c34a3fff1c3
GareusMno <58163135+GareusMno@users.noreply.github.com>
2021-02-19 17:07:45 +0100

    Creacion del proyecto
    Sala 1- Testeo de movimiento, salto y colisiones del jugador

---
+++ UnityCode/Gambling thief/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+    public bool isGrounded = false;
+    public float Velocidad = 400f;
+    public float Salto = 200f;
+    // Start is called before the first frame update
+    void Start(){
+        if (Instance != null){
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left")){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Velocidad * Time.deltaTime,0));
+            gameObject.GetComponent<SpriteRenderer>().flipX=true;
+        }
+
+        if(Input.GetKey("right")){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Velocidad * Time.deltaTime,0));
+            gameObject.GetComponent<SpriteRenderer>().flipX=false;
+        }
+        
+        if (!Input.GetKey("right")&&!Input.GetKey("left")){
+        }
+
+        if(Input.GetKeyDown("up") && isGrounded){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,Salto));
+            Debug.Log("Salto");
+        }
+    }
+}
