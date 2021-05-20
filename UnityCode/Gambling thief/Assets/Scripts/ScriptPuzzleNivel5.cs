d451da03c067e0e8a84ff568b398f0609ca61154
GareusMno <58163135+GareusMno@users.noreply.github.com>
2021-05-20 14:43:32 +0200

    Nueva build, por terminar nvl 6

---
+++ UnityCode/Gambling thief/Assets/Scripts/ScriptPuzzleNivel5.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPuzzleNivel5 : MonoBehaviour
+{
+    public GameObject Piedra1,Piedra2,Base1,Base2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
